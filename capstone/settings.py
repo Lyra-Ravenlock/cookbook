13a815208d71884f646a808de2797367e337c0a9
S. Myrina Petrocheilou <95349917+Lyra-Ravenlock@users.noreply.github.com>
2025-06-25 13:23:33 +0300

    added logging for render

--- capstone/settings.py
+++ capstone/settings.py
@@ -133,3 +133,16 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+}
